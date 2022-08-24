0b2650e0acd29b80d971f04f16c2b92ba6e0dd09
Takinur M <takinurm@gmail.com>
2022-08-24 11:53:07 +0600

    Password Hash Added

--- Backend/base/api/serializers.py
+++ Backend/base/api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -9,18 +10,20 @@ class UserCreateSerializer(serializers.ModelSerializer):
        fields = ('id', 'name', 'email', 'password')
        extra_kwargs = {'password': {'write_only': True}}
        
-    def validate(self, data):
-        try:
-            validate_password(data['password'])
+    # Validate password and return Hashed password
+    def valilidate(self, data):
+        password = data['password']
+        try :
+            validate_password(password)
         except serializers.ValidationError as e:
             raise serializers.ValidationError({'password': e.messages})
         
+        # data['password'] = make_password(password)
+        
         return data
        
-def create(self, validated_data):
-    user = User.objects.create_user(**validated_data)
-    
-    user.save()
-    
-    return user
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+            
+        return user
 
--- Backend/base/api/views.py
+++ Backend/base/api/views.py
@@ -16,7 +16,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
 
         # Add custom claims
-        token['username'] = user.username
+        token['name'] = user.name
         # ...
 
         return token
