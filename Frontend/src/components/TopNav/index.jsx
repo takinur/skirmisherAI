6cfd9da47c8520e21440c073dbe96b5fa40f72ad
Takinur M <takinurm@gmail.com>
2022-09-08 23:10:31 +0600

    removed subsections

--- Frontend/src/components/TopNav/index.jsx
+++ Frontend/src/components/TopNav/index.jsx
@@ -15,9 +15,9 @@ const TopNavigation = () => {
       <HashtagIcon />
       <Title />
       <ThemeIcon />
-      <Search />
+      
       <BellIcon />
-      <UserCircle />
+   
     </div>
   );
 };
@@ -36,16 +36,8 @@ const ThemeIcon = () => {
   );
 };
 
-const Search = () => (
-  <div className="search">
-    <input className="search-input" type="text" placeholder="Search..." />
-    <FaSearch size="18" className="text-secondary my-auto" />
-  </div>
-);
 const BellIcon = () => <FaRegBell size="24" className="top-navigation-icon" />;
-const UserCircle = () => (
-  <FaUserCircle size="24" className="top-navigation-icon" />
-);
+
 const HashtagIcon = () => <FaHashtag size="20" className="title-hashtag" />;
 const Title = () => <h5 className="title-text">tailwind-css</h5>;
 
