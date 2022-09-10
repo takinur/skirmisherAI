62452a114f18fa018e6e1990c69dc3f29335049a
Takinur M <takinurm@gmail.com>
2022-09-11 00:15:08 +0600

    Refactored scroll

--- Frontend/src/components/ScrollToTop/index.jsx
+++ Frontend/src/components/ScrollToTop/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-
-import "./ScrollToTop.css";
+import './index.css'
 
 const ScrollToTop = () => {
   const [showTopBtn, setShowTopBtn] = useState(false);
--- Frontend/src/components/UserMenu.jsx
+++ Frontend/src/components/UserMenu.jsx
@@ -182,3 +182,4 @@ function IconThree() {
     </svg>
   );
 }
+
