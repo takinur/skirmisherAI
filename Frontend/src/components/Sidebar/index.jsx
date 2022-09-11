52b4b9f0928f8824b556156f3898304f62fa4ddf
Takinur M <takinurm@gmail.com>
2022-09-12 01:40:37 +0600

    Conditional Menu

--- Frontend/src/components/Sidebar/index.jsx
+++ Frontend/src/components/Sidebar/index.jsx
@@ -1,9 +1,58 @@
 import React from "react";
 import ClassNames from "classnames";
 import { Link, NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const SideBar = ({ navbar, toggleNavbar }) => {
-  console.log("from sidebar", navbar);
+  const { user } = useSelector((state) => state.auth);
+
+  let MenuItems = [];
+  //Conditionally render menu items based on user role
+  if (user.role === "EMPLOYER") {
+    MenuItems = [
+      {
+        name: "Dashboard",
+        link: "/dashboard",
+        icon: IconOne,
+      },
+      {
+        name: "Setting",
+        description: "Change your password and other settings",
+        link: "/user/setting",
+        icon: IconTwo,
+      },
+      {
+        name: "Help",
+        description: "Get help with using the platform",
+        link: "/help",
+        icon: IconThree,
+      },
+    ];
+  } else if (user.role === "CANDIDATE") {
+    MenuItems = [
+      {
+        name: "Profile",
+        description: "View and modify your profile",
+        href: "/user/profile",
+        icon: IconThree,
+      },
+      {
+        name: "Setting",
+        description: "Change your password and other settings",
+        href: "/user/setting",
+        icon: IconTwo,
+      },
+      {
+        name: "Help",
+        description: "Get help with using the platform",
+        href: "/help",
+        icon: IconThree,
+      },
+    ];
+  }
+
+  console.table(MenuItems);
+  // console.log("from sidebar", menu);
   //TODO: Render menu items from a Array of objects
   //Change icons
   //Create profile section
@@ -19,7 +68,7 @@ const SideBar = ({ navbar, toggleNavbar }) => {
         <div className="w-full flex justify-between pr-2">
           <Link
             className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
-            to='/dashboard'
+            to="/dashboard"
           >
             SkirmisherAI
           </Link>
@@ -40,26 +89,26 @@ const SideBar = ({ navbar, toggleNavbar }) => {
           </svg>
         </div>
         <ul className="mt-6">
-          <li className="relative px-6 py-3">
-            <a
-              className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-              href="/app/dashboard"
-            >
-              <svg
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="w-5 h-5"
-                aria-hidden="true"
+          {MenuItems.map((item) => (
+            <li className="relative px-6 py-3" key={item.name}>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "[--child-opacity:1] side-nav-link"
+                    : " side-nav-link"
+                }
+                to={item.link}
               >
-                <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
-              </svg>
-              <span className="ml-4">Dashboard</span>
-            </a>
-          </li>
+                <span
+                  className="opacity-[var(--child-opacity,0)] absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                  aria-hidden="true"
+                ></span>
+                <item.icon aria-hidden="true" />
+
+                <span className="ml-4">{item.name}</span>
+              </NavLink>
+            </li>
+          ))}
           <li className="relative px-6 py-3">
             <NavLink
               to="/user/profile"
@@ -243,3 +292,54 @@ const SideBar = ({ navbar, toggleNavbar }) => {
 };
 
 export default SideBar;
+
+function IconOne() {
+  return (
+    <svg
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className="w-5 h-5"
+      aria-hidden="true"
+    >
+      <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
+    </svg>
+  );
+}
+
+function IconTwo() {
+  return (
+    <svg
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className="w-5 h-5"
+      aria-hidden="true"
+    >
+      <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
+    </svg>
+  );
+}
+
+function IconThree() {
+  return (
+    <svg
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className="w-5 h-5"
+    aria-hidden="true"
+  >
+    <path d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path>
+  </svg>
+  );
+}
