887518f84c9df39fd35b68ebaf5780ece4878b30
Takinur M <takinurm@gmail.com>
2022-09-12 13:31:47 +0600

    Link for Homepage

--- Frontend/src/pages/Error/index.jsx
+++ Frontend/src/pages/Error/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ChatWindow } from "../../components/Message";
 import notFound from "../../assets/images/404.svg";
 import { FaChevronCircleLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export const NotFound = () => {
   return (
@@ -21,14 +22,14 @@ export const NotFound = () => {
               <p>You can find plenty of other things on our homepage.</p>
             </div>
             <div className="mb-20 md:mb-0">
-              <button
+              <Link
+                to="/"
                 type="button"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 <FaChevronCircleLeft className="mr-2" />
                 Back to Home Page
-              </button>
-             
+              </Link>
             </div>
           </div>
           <div className="w-full md:w-1/2 text-center bg-green-400">
