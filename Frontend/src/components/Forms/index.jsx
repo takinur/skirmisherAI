7cfeb02a66df20213cc781a4c95678417f414e54
Takinur M <takinurm@gmail.com>
2022-09-14 02:12:42 +0600

    Select Dropdown

--- Frontend/src/components/Forms/index.jsx
+++ Frontend/src/components/Forms/index.jsx
@@ -3,36 +3,40 @@ import classNames from "classnames";
 import Label from "../Label";
 import Input from "../Input";
 import ButtonDefault from "../ButtonDefault";
+import { SelectDropDown } from "../SelectDropdown";
 
 export const EmpProfileForm = ({handleSubmit, submitForm, register, isLoading}) => {
   return (
     <div className="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
       <div className="w-full sm:max-w-lg mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         <div className="text-center mb-7">
-          <h2 className="text-3xl">Finish Confirming details!</h2>
+          <h2 className="text-3xl">Finish confirming the details!</h2>
         </div>
         <form onSubmit={handleSubmit(submitForm)}>
           <div>
-            <Label htmlFor="email">Email</Label>
+            <Label htmlFor="designation">Organization / Company</Label>
+            <SelectDropDown />
+          </div>
+          <div className="mt-4">
+            <Label htmlFor="designation">Designation</Label>
             <Input
-              id="email"
-              type="email"
+              id="designation"
+              type="text"
               className="mt-1 block w-full"
-              {...register("email")}
+              {...register("designation")}
               required
               autoFocus
             />
           </div>
 
           <div className="mt-4">
-            <Label htmlFor="password">Password</Label>
+            <Label htmlFor="phone">Phone</Label>
             <Input
-              id="password"
-              type="password"
+              id="phone"
+              type="text"
               className="mt-1 block w-full"
-              {...register("password")}
+              {...register("phone")}
               required
-              autoComplete="current-password"
             />
           </div>
           <div className="mt-4 flex justify-end">
