f3b5966543d24f8f08283378a64b66e5a88bd8b5
Takinur M <takinurm@gmail.com>
2022-09-14 23:02:25 +0600

    Profile Design

--- Frontend/src/pages/Auth/UserProfile.jsx
+++ Frontend/src/pages/Auth/UserProfile.jsx
@@ -1,19 +1,11 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import AuthLayout from "../Layout/Auth";
-import { useAxiosPrivate } from "../../hooks/useAxiosPrivate";
-import { useQuery } from "react-query";
-import { EmpProfileForm } from "../../components/Forms";
 
 import { Profile as EmpProfile } from "../Employer/profile";
 
 export default function UserProfile() {
   const { user } = useSelector((state) => state.auth);
-
-  // const roleBasedRender = () => {
-  //   // if (user.role === "EMPLOYER") return <EmpProfileForm user={user} />;
-  //   if (user.role === "EMPLOYER") return <EmpProfile user={user} />;
-  // };
 
   const RoleView = {
     EMPLOYER: <EmpProfile user={user} />,
