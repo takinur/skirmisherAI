43fe2c62a73ad102b1bbe028fdf315c14e0d315d
Takinur M <takinurm@gmail.com>
2022-09-20 02:37:43 +0600

    Adeded skills at frontend

--- Frontend/src/pages/Candidate/profile.jsx
+++ Frontend/src/pages/Candidate/profile.jsx
@@ -4,31 +4,42 @@ import { useAxiosPrivate } from "../../hooks/useAxiosPrivate";
 
 import { CandProfileForm } from "../../components/Forms/Candidate";
 import { Loading } from "../../components/Loading";
-import { FaBuilding, FaGlobe, FaMapMarkerAlt,  FaPhoneAlt, FaUsers } from "react-icons/fa";
+import {
+  FaBuilding,
+  FaGlobe,
+  FaMapMarkerAlt,
+  FaPhoneAlt,
+  FaUsers,
+} from "react-icons/fa";
 
 export const Profile = (props) => {
   const API = useAxiosPrivate();
 
   //React query to fetch profile
-  const { isLoading, isError, error, data } = useQuery("candProfile", fetchProfile, {
-    refetchOnWindowFocus: false,
-    retry: 0,
-  });
+  const { isLoading, isError, error, data } = useQuery(
+    "candProfile",
+    fetchProfile,
+    {
+      refetchOnWindowFocus: false,
+      retry: 0,
+    }
+  );
   //Async function to fetch profile
   async function fetchProfile() {
     const res = await API.get(`/account/candidate/${props.user.id}`);
     return res.data;
   }
 
-  console.log(data);
+  console.log("Data from cand profile: ", data);
 
   if (isLoading) return <Loading />;
-  if (isError && error.request.status === 400) return <CandProfileForm user={props.user} />;
+  if (isError && error.request.status === 400)
+    return <CandProfileForm user={props.user} />;
   //Return detailed profile view
   if (data) return DetailedProfileView(data, props.user);
 };
 
-function DetailedProfileView(company, user) {
+function DetailedProfileView(profileData, user) {
   return (
     <>
       <section className="relative py-8">
@@ -48,10 +59,10 @@ function DetailedProfileView(company, user) {
                 <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                   <div className="py-6 px-3 mt-32 sm:mt-0">
                     <button
-                      className="bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                      className="bg-green-500 active:bg-green-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                       type="button"
                     >
-                      Connect
+                      View Resume
                     </button>
                   </div>
                 </div>
@@ -83,41 +94,47 @@ function DetailedProfileView(company, user) {
                   {user.name}
                 </h3>
                 <div className="flex justify-center text-sm leading-normal mt-0 mb-2 text-gray-400 font-bold uppercase">
-                  <FaBuilding className="mr-2 text-xl text-gray-400" />
-                  {company.company_name}
+                  {profileData.designation}
                 </div>
-                <div className="flex justify-center text-sm leading-normal mt-0 mb-2 text-gray-400 font-bold">
-                  <FaMapMarkerAlt className="mr-2 text-xl text-gray-400" />
-                  {company.location}
-                </div>
-                <div className="mb-2 text-green-700 text-xl mt-10">
-                  {company.slogan}
+                <div className="flex justify-center text-sm leading-normal mt-6 mb-2 text-gray-500 font-bold">
+                  <FaMapMarkerAlt className="mr-1 text-xl text-gray-400" />
+                  {profileData.location ? profileData.location : "Not Provided"}
                 </div>
                 <div className="mt-2 grid items-center justify-center">
                   <div className="flex">
-                    <FaGlobe className="mr-2 mt-1 text-zinc-500 font-bold" />                 
+                    <FaGlobe className="mr-1 mt-1 text-zinc-500 font-bold" />
                     <a
                       className=" font-semibold text-blue-700 hover:opacity-90"
                       target="_blank"
-                      href={company.website}
+                      href={profileData.website ? profileData.website : "#"}
                     >
-                      {company.website}
+                      {profileData.website
+                        ? profileData.website
+                        : "No website/ Portfolio"}
                     </a>
                   </div>
                 </div>
-                <div className="flex justify-center text-sm leading-normal mt-4 mb-2 text-gray-600 font-bold">
-                  <FaUsers className="mr-2 text-xl text-gray-600" />{company.size}
-                </div>
-                <div className="flex justify-center text-sm leading-normal mt-4 mb-2 text-gray-600 font-bold">
-                  <FaPhoneAlt className="mr-2 text-lg text-gray-600" />{company.phone}
+                <div className="flex justify-center text-sm leading-normal mt-4 mb-2 text-gray-500 font-bold">
+                  <FaPhoneAlt className="mr-2 text-lg text-gray-500" />
+                  {profileData.phone ? profileData.phone : "Not Provided"}
                 </div>
               </div>
-              <div className="mt-10 py-10 border-t border-gray-200 text-center">
-                <div className="flex flex-wrap justify-center">
-                  <div className="w-full lg:w-9/12 px-4">
-                    <p className="mb-4 text-lg leading-relaxed text-gray-700">
-                      {company.about}
-                    </p>
+              <div className="mt-10 py-10 border-t border-gray-200 w-full grid">
+                <div className="mx-auto border-l-8 border-green-700 px-3 bg-green-50 ">
+                  <h3 className="text-2xl font-semibold leading-normal text-gray-700 mb-2 ">                    
+                    Your Skills*
+                  </h3>
+                </div>
+                <div className="flex flex-col w-full ">
+                  <div className="p-1  mt-2 text-center space-x-1 space-y-2">
+                    {profileData.skills.map((skill) => (
+                      <button
+                        key={skill.id}
+                        className="bg-gray-300 px-5 py-2 text-sm shadow-sm font-medium tracking-wider  text-gray-600 rounded-full hover:shadow-2xl hover:bg-gray-400"
+                      >
+                        {skill.name}
+                      </button>
+                    ))}
                   </div>
                 </div>
               </div>
