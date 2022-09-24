391402c64b28f62bda5ae88ad98b6693b2964649
Takinur M <takinurm@gmail.com>
2022-09-25 02:47:47 +0600

    Pagination

--- Frontend/src/components/Table/index.jsx
+++ Frontend/src/components/Table/index.jsx
@@ -22,75 +22,128 @@ export const Table = ({ ...props }) => {
   });
 
   return (
-    <>
-      <div className="min-w-screen min-h-screen">
-        <div className="w-full bg-gray-100">
-          <div className="bg-hite w-full shadow-md rounded my-6 bg-gray-100">
-            <table className="min-w-max w-full table-auto">
-              <thead>
-                {table.getHeaderGroups().map((headerGroup) => (
-                  <tr
-                    key={headerGroup.id}
-                    className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal"
-                  >
-                    {headerGroup.headers.map((header) => {
-                      return (
-                        <th
-                          key={header.id}
-                          colSpan={header.colSpan}
-                          className="py-3 px-6 text-left"
-                        >
-                          {header.isPlaceholder ? null : (
-                            <div>
-                              {flexRender(
-                                header.column.columnDef.header,
-                                header.getContext()
-                              )}
-                              {header.column.getCanFilter() ? (
-                                <div>
-                                  <Filter
-                                    column={header.column}
-                                    table={table}
-                                  />
-                                </div>
-                              ) : null}
-                            </div>
-                          )}
-                        </th>
-                      );
-                    })}
-                  </tr>
-                ))}
-              </thead>
-              <tbody class="text-gray-600 ">
-                {table.getRowModel().rows.map((row) => {
+    <div className="overflow-hidden ">
+      <div className="w-full shadow-md rounded my-6 bg-white overflow-x-auto">
+        <table className="min-w-max w-full table-auto">
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr
+                key={headerGroup.id}
+                className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal"
+              >
+                {headerGroup.headers.map((header) => {
                   return (
-                    <tr
-                      key={row.id}
-                      className="border-b border-gray-200 hover:bg-gray-100"
+                    <th
+                      key={header.id}
+                      colSpan={header.colSpan}
+                      className="py-3 px-6 text-left"
                     >
-                      {row.getVisibleCells().map((cell) => {
-                        return (
-                          <td
-                            key={cell.id}
-                            className="py-3 px-6 text-left whitespace-nowrap"
-                          >
-                            {flexRender(
-                              cell.column.columnDef.cell,
-                              cell.getContext()
-                            )}
-                          </td>
-                        );
-                      })}
-                    </tr>
+                      {header.isPlaceholder ? null : (
+                        <div>
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                          {header.column.getCanFilter() ? (
+                            <div>
+                              <Filter column={header.column} table={table} />
+                            </div>
+                          ) : null}
+                        </div>
+                      )}
+                    </th>
                   );
                 })}
-              </tbody>
-            </table>
-          </div>
+              </tr>
+            ))}
+          </thead>
+          <tbody className="text-gray-600 ">
+            {table.getRowModel().rows.map((row) => {
+              return (
+                <tr
+                  key={row.id}
+                  className="border-b border-gray-200 hover:bg-gray-100"
+                >
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <td
+                        key={cell.id}
+                        className="py-3 px-6 text-left whitespace-nowrap"
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex flex-col items-center pb-4">
+        <span className="text-sm text-gray-700 dark:text-gray-400">
+          Showing
+          <span className="font-semibold text-gray-900 dark:text-white mx-1">
+            {table.getRowModel().rows.length}
+          </span>
+           out of
+          <span className="font-semibold text-gray-900 dark:text-white mx-1">
+            {table.getRowModel().rows.length}
+          </span>
+          Entries
+        </span>
+        <div className="inline-flex mt-2 xs:mt-0">
+          <button
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+            className="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-l
+           hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white
+           disabled:opacity-50 disabled:cursor-default
+           "
+          >
+            <svg
+              aria-hidden="true"
+              className="mr-2 w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            Prev
+          </button>
+          <button
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+            className="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-r border-0 border-l border-gray-700 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white
+            disabled:opacity-50 disabled:cursor-default
+            "
+          >
+            Next
+            <svg
+              aria-hidden="true"
+              className="ml-2 w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
