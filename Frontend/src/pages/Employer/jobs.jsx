8e4a267be9047871638eec8aa2660942485adbbf
Takinur M <takinurm@gmail.com>
2022-09-29 01:10:32 +0600

    Modal for Deleting Jobs

--- Frontend/src/pages/Employer/jobs.jsx
+++ Frontend/src/pages/Employer/jobs.jsx
@@ -9,6 +9,8 @@ import { Link } from "react-router-dom";
 import { NoExInfo } from "../../components/Alerts";
 
 import { useProfile } from "../../hooks/useProfile";
+import HeadlessModal from "../../components/Modal";
+import { useState } from "react";
 
 export const jobs = () => {
   const API = useAxiosPrivate();
@@ -39,6 +41,11 @@ export const jobs = () => {
   );
 
   // console.log("Jobs;", data);
+  
+  //Delete Method
+  const handleDelete = (id) =>{
+    console.log('deete', id)
+  }
 
   //Conditional rendering
   const renderDetails = () => {
@@ -138,11 +145,22 @@ export const jobs = () => {
     },
   ];
 
-  return <AuthLayout title="Manage Job Posting">{renderDetails()}</AuthLayout>;
+  const [isModal, setModal] = useState(false);
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
+  return (
+    <AuthLayout title="Manage Job Posting">
+      {renderDetails()}
+      <ConfirmModal isModal={isModal} closeModal={closeModal} />
+    </AuthLayout>
+  );
 };
 
 //Actions column
-function Actions({ row, employer }) {
+function Actions({ row, employer, handleDelete }) {
   return (
     <div className="flex item-center">
       <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
@@ -201,5 +219,57 @@ function Actions({ row, employer }) {
         </svg>
       </div>
     </div>
+  );
+}
+
+function ConfirmModal({ isModal, closeModal }) {
+  return (
+    <HeadlessModal isOpen={isModal} closeModal={closeModal}>
+      <div className="text-center p-5 flex-auto justify-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-4 h-4 -m-1 flex items-center text-red-500 mx-auto"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-16 h-16 flex items-center text-red-500 mx-auto"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        <h2 className="text-xl font-bold py-4 ">Are you sure?</h2>
+        <p className="text-sm text-gray-500 px-8">
+          Do you really want to delete your Job posting? This process cannot be
+          undone
+        </p>
+      </div>
+
+      <div className="p-3  mt-2 text-center space-x-4 md:block">
+        <button
+          onClick={closeModal}
+          className="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100"
+        >
+          Cancel
+        </button>
+        <button className="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">
+          Delete
+        </button>
+      </div>
+    </HeadlessModal>
   );
 }
