95ea84f9b2a5d196b0029dc2d8d1566d80b29f91
Takinur M <takinurm@gmail.com>
2022-10-02 00:06:07 +0600

    Extracted Component

--- Frontend/src/pages/Jobs.jsx
+++ Frontend/src/pages/Jobs.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaClock } from "react-icons/fa";
+import { JobCard } from "../components/Card";
 import GuestLayout from "./Layout/Guest";
 
 export const Jobs = () => {
@@ -48,68 +49,14 @@ export const Jobs = () => {
           </div>
         </div>
         <div className="mt-10 w-full">
-          <div className="relative w-full cursor-pointer overflow-hidden rounded bg-gray-50 shadow-lg hover:bg-inherit">
-            <div className="px-6 py-4">
-              <div className="header flex justify-between text-gray-700">
-                <div className="mb-2 text-xl font-bold">Web Developer</div>
-                <div className="mb-2">
-                  <div className="right-0 top-0 h-8 w-8 rounded-full border-2 border-gray-500 hover:bg-gray-50">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="m-auto mt-1 h-5 w-5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                      />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-              <div className=" text-gray-700">
-                <div className="mb-2 text-sm font-semibold">
-                  <span className="text-gray-700">Google, California</span>
-                </div>
-                <p className="text-base text-gray-700">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                  exercitationem praesentium nihil.
-                </p>
-              </div>
-              <div className="mt-2">
-                <div className="flex justify-between text-gray-700">
-                  <span className="text-md hidden bg-gray-200  px-3 py-1 font-medium text-gray-800 md:block">
-                    Remote
-                  </span>
-                  <div className="time flex">
-                    {" "}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="h-5 w-5 mt-1"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                      />
-                    </svg>
-                    12h ago
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
         </div>
       </div>
     </GuestLayout>
   );
 };
+
+   
