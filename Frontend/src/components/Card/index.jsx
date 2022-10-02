4843ed634d1a1ffa0ddc6ad247b96a2c3ee1f2f7
Takinur M <takinurm@gmail.com>
2022-10-02 18:22:29 +0600

    Overview card

--- Frontend/src/components/Card/index.jsx
+++ Frontend/src/components/Card/index.jsx
@@ -2,77 +2,77 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const JobCard = () => {
+  const handleSave = () => {
+    console.log("Saved");
+  };
 
-
-    const handleSave = () => {
-        
-        console.log("Saved");
-    };
+  const handleItemClick = () => {
+    console.log("Clicked");
+  };
 
   return (
-    <div
-      className="relative mt-2 w-full cursor-pointer overflow-hidden rounded bg-gray-50 shadow-lg hover:bg-inherit"
-    >
-      <Link to="/find-work/125">
-        <div className="px-6 py-4">
-          <div className="header flex justify-between text-gray-700">
-            <div className="mb-2 text-xl font-bold">Web Developer</div>
-            <div className="mb-2 z-20 relative">
-              <button className="right-0 top-0 h-8 w-8 rounded-full border-2 border-gray-500 hover:bg-gray-50" onClick={handleSave}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="m-auto h-5 w-5"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                  />
-                </svg>
-              </button>
-            </div>
+    <div className="relative mt-2 w-full cursor-pointer overflow-hidden rounded bg-gray-50 shadow-lg hover:bg-inherit">
+      <div className="px-6 py-4">
+        <div className="header flex justify-between text-gray-700">
+          <div className="mb-2 text-xl font-bold">Web Developer</div>
+          <div className="relative z-20 mb-2">
+            <button
+              className="absolute right-0 top-0 h-8 w-8 rounded-full border-2 border-gray-500 hover:bg-gray-50"
+              onClick={handleSave}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="m-auto h-5 w-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                />
+              </svg>
+            </button>
           </div>
-          <div className=" text-gray-700">
-            <div className="mb-2 text-sm font-semibold">
-              <span className="text-gray-700">Google, California</span>
-            </div>
-            <p className="text-base text-gray-700">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
+        </div>
+        <div className=" text-gray-700">
+          <div className="mb-2 text-sm font-semibold">
+            <span className="text-gray-700">Google, California</span>
           </div>
-          <div className="mt-2">
-            <div className="flex justify-between text-gray-700">
-              <span className="text-md  bg-gray-200  px-3 py-1 font-medium text-gray-800 md:block">
-                Remote
-              </span>
-              <div className="time flex">
-                {" "}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="mt-1 h-5 w-5"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-                12h ago
-              </div>
+          <p className="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div className="mt-2">
+          <div className="flex justify-between text-gray-700">
+            <span className="text-md  bg-gray-200  px-3 py-1 font-medium text-gray-800 md:block">
+              Remote
+            </span>
+            <div className="time flex">
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="mt-1 h-5 w-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              12h ago
             </div>
           </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
--- Frontend/src/pages/Jobs.jsx
+++ Frontend/src/pages/Jobs.jsx
@@ -3,6 +3,8 @@ import { FaClock } from "react-icons/fa";
 import { JobCard } from "../components/Card";
 import GuestLayout from "./Layout/Guest";
 
+import "./demo.css";
+
 export const Jobs = () => {
   return (
     <GuestLayout>
@@ -49,14 +51,195 @@ export const Jobs = () => {
           </div>
         </div>
         <div className="mt-10 w-full">
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
+          <div className="detail-page">
+            <div className="flex flex-grow flex-col pl-10">
+              <div className="flex justify-between">
+                <div className="text-sm font-semibold text-gray-800 ">
+                  Showing 46 Jobs
+                </div>
+                <div className="text-sm font-semibold text-gray-800">
+                  Sort by: <span className="post-time">Newest Post </span>
+                  <span className="menu-icon">▼</span>
+                </div>
+              </div>
+              <div className="flex flex-grow mt-4">
+                <div className="flex w-2/5 flex-col bg-red-400 ">
+                  <div className="bg-green-600">
+                    <div className="py-5 px-4 w-full cursor-pointer rounded-md bg-indigo-500 ">
+                      <div className="overview-wrapper">
+                        <svg
+                          viewBox="0 -13 512 512"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g fill="#feb0a5">
+                            <path d="M256 92.5l127.7 91.6L512 92 383.7 0 256 91.5 128.3 0 0 92l128.3 92zm0 0M256 275.9l-127.7-91.5L0 276.4l128.3 92L256 277l127.7 91.5 128.3-92-128.3-92zm0 0" />
+                            <path d="M127.7 394.1l128.4 92 128.3-92-128.3-92zm0 0" />
+                          </g>
+                          <path
+                            d="M512 92L383.7 0 256 91.5v1l127.7 91.6zm0 0M512 276.4l-128.3-92L256 275.9v1l127.7 91.5zm0 0M256 486.1l128.4-92-128.3-92zm0 0"
+                            fill="#feb0a5"
+                          />
+                        </svg>
+                        <div className="overview-detail">
+                          <div className="job-card-title">UI / UX Designer</div>
+                          <div className="job-card-subtitle">
+                            2972 Westheimer Rd. Santa Ana.
+                          </div>
+                        </div>
+                        <svg
+                          className="heart"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path d="M20.8 4.6a5.5 5.5 0 00-7.7 0l-1.1 1-1-1a5.5 5.5 0 00-7.8 7.8l1 1 7.8 7.8 7.8-7.7 1-1.1a5.5 5.5 0 000-7.8z" />
+                        </svg>
+                      </div>
+                      <div className="job-overview-buttons">
+                        <div className="search-buttons time-button">
+                          Full Time
+                        </div>
+                        <div className="search-buttons level-button">
+                          Senior Level
+                        </div>
+                        <div className="job-stat">New</div>
+                        <div className="job-day">4d</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-3/5">
+                  <img className="job-bg" alt="" />
+                  <div className="job-logos">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                      <path
+                        xmlns="http://www.w3.org/2000/svg"
+                        d="M0 .5h4.2v23H0z"
+                        fill="#042b48"
+                        data-original="#212121"
+                      ></path>
+                      <path
+                        xmlns="http://www.w3.org/2000/svg"
+                        d="M15.4.5a8.6 8.6 0 100 17.2 8.6 8.6 0 000-17.2z"
+                        fill="#fefefe"
+                        data-original="#f4511e"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="job-explain-content">
+                    <div className="job-title-wrapper">
+                      <div className="job-card-title">UI /UX Designer</div>
+                      <div className="job-action">
+                        <svg
+                          className="heart"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path d="M20.8 4.6a5.5 5.5 0 00-7.7 0l-1.1 1-1-1a5.5 5.5 0 00-7.8 7.8l1 1 7.8 7.8 7.8-7.7 1-1.1a5.5 5.5 0 000-7.8z" />
+                        </svg>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="feather feather-share-2"
+                        >
+                          <circle cx="18" cy="5" r="3" />
+                          <circle cx="6" cy="12" r="3" />
+                          <circle cx="18" cy="19" r="3" />
+                          <path d="M8.6 13.5l6.8 4M15.4 6.5l-6.8 4" />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="job-subtitle-wrapper">
+                      <div className="company-name">
+                        Patreon{" "}
+                        <span className="comp-location">Londontowne, MD.</span>
+                      </div>
+                      <div className="posted">
+                        Posted 8 days ago
+                        <span className="app-number">98 Application</span>
+                      </div>
+                    </div>
+                    <div className="explain-bar">
+                      <div className="explain-contents">
+                        <div className="explain-title">Experience</div>
+                        <div className="explain-subtitle">Minimum 1 Year</div>
+                      </div>
+                      <div className="explain-contents">
+                        <div className="explain-title">Work Level</div>
+                        <div className="explain-subtitle">Senior level</div>
+                      </div>
+                      <div className="explain-contents">
+                        <div className="explain-title">Employee Type</div>
+                        <div className="explain-subtitle">Full Time Jobs</div>
+                      </div>
+                      <div className="explain-contents">
+                        <div className="explain-title">Offer Salary</div>
+                        <div className="explain-subtitle">$2150.0 / Month</div>
+                      </div>
+                    </div>
+                    <div className="overview-text">
+                      <div className="overview-text-header">Overview</div>
+                      <div className="overview-text-subheader">
+                        We believe that design (and you) will be critical to the
+                        company's success. You will work with our founders and
+                        our early customers to help define and build our product
+                        functionality, while maintaining the quality bar that
+                        customers have come to expect from modern SaaS
+                        applications. You have a strong background in product
+                        design with a quantitavely anf qualitatively analytical
+                        mindset. You will also have the opportunity to craft our
+                        overall product and visual identity and should be
+                        comfortable to flex into working.
+                      </div>
+                    </div>
+                    <div className="overview-text">
+                      <div className="overview-text-header">
+                        Job Description
+                      </div>
+                      <div className="overview-text-item">
+                        3+ years working as a product designer.
+                      </div>
+                      <div className="overview-text-item">
+                        A portfolio that highlights your approach to problem
+                        solving, as well as you skills in UI.
+                      </div>
+                      <div className="overview-text-item">
+                        Experience conducting research and building out smooth
+                        flows.
+                      </div>
+                      <div className="overview-text-item">
+                        Excellent communication skills with a well-defined
+                        design process.
+                      </div>
+                      <div className="overview-text-item">
+                        Familiarity with design tools like Sketch and Figma
+                      </div>
+                      <div className="overview-text-item">
+                        Up-level our overall design and bring consistency to
+                        end-user facing properties
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </GuestLayout>
   );
 };
-
-   
