5fc57a9333abb1e7c4a4fb5c038f6bd82c3d066a
Takinur M <takinurm@gmail.com>
2022-10-07 01:59:11 +0600

    Fetched Job details at front-end

--- Frontend/src/pages/Employer/JobDetails.jsx
+++ Frontend/src/pages/Employer/JobDetails.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import AuthLayout from "../Layout/Auth";
+import { useQuery } from "react-query";
+import { useAxiosPrivate } from "../../hooks/useAxiosPrivate";
+
 
 export const JobDetails = () => {
   const navigate = useNavigate();
+  const API = useAxiosPrivate()
+
+
   const { id } = useParams();
 
+  if(!id) {
+    navigate("/employer/jobs");
+  }
+
+  //Fetch Job Applications 
+  const { isLoading, data, refetch } = useQuery(
+    "applications",
+    async () => {
+      const res = await API.get(`/v1/application-dashboard/?job_id=${id}`);
+      return res.data;
+    },
+    {
+      refetchOnWindowFocus: true,
+      retry: 1,
+    }
+  );
+
+  console.log('Feteched', data);
+
+  
 
 
   //TODO: Fetch Job Details
