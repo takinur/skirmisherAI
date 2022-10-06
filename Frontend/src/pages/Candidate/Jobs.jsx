f0e92d39db3de61614d853f4e04217010ebb0803
Takinur M <takinurm@gmail.com>
2022-10-06 16:55:58 +0600

    List of applied Jobs with ID

--- Frontend/src/pages/Candidate/Jobs.jsx
+++ Frontend/src/pages/Candidate/Jobs.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useQuery } from "react-query";
-import { Applied } from "../../components/Card/applied";
 import { Loading } from "../../components/Loading";
 import { useAxiosPrivate } from "../../hooks/useAxiosPrivate";
 import { useCandProfile } from "../../hooks/useProfile";
 import AuthLayout from "../Layout/Auth";
+import { relativeTime } from "../../hooks/useRelativetime";
 
 export const Jobs = () => {
   const API = useAxiosPrivate();
@@ -38,14 +38,63 @@ export const Jobs = () => {
         <h2 className="mb-4 text-center text-2xl font-bold">Applied Jobs</h2>
 
         <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          <Applied />
-          <Applied />
-          <Applied />
-          <Applied />
-          <Loading />
-          <Loading />
+          {isLoading ? (
+            <>
+              <Loading />
+              <Loading />
+              <Loading />
+              <Loading />
+            </>
+          ) : (
+            data?.map((job, index) => (
+              <div
+                key={job.id}
+                className="relative flex items-start rounded-xl bg-white p-4 shadow-lg"
+              >
+                <div className="absolute bottom-0 right-0 rounded-tl-xl rounded-br-xl bg-green-600 px-2 py-1 text-white">
+                  <span className="text-xs">{job.status}</span>
+                </div>
+
+                <div className="my-auto flex h-12 w-12 items-center justify-center rounded-full border border-blue-100 bg-blue-50">
+                  <span className="text-3xl font-bold text-blue-500">{
+                    // show index in reverse order
+                    data.length - index
+                    
+                  }</span>
+                </div>
+
+                <div className="ml-4 mt-1">
+                  <h2 className="font-semibold">{job.job_title}</h2>
+                  <p className="text-sm text-gray-500">{job.employer}</p>
+                  <p className="mt-2 text-sm text-gray-500">
+                    Applied {relativeTime(job.created_at)}
+                  </p>
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </div>
     </AuthLayout>
   );
 };
+
+function appliedJobs({}) {
+  return (
+    <div className="relative flex items-start rounded-xl bg-white p-4 shadow-lg">
+      <div className="absolute bottom-0 right-0 rounded-tl-xl rounded-br-xl bg-green-600 px-2 py-1 text-white">
+        <span className="text-xs">{job.status}</span>
+      </div>
+
+      <div className="my-auto flex h-12 w-12 items-center justify-center rounded-full border border-blue-100 bg-blue-50">
+        <span className="text-3xl font-bold text-blue-500">1</span>
+      </div>
+
+      <div className="ml-4 mt-1">
+        <h2 className="font-semibold">{job.job_title}</h2>
+        <p className="text-sm text-gray-500">{job.employer}</p>
+        <p className="mt-2 text-sm text-gray-500">Applied {job.created_at}</p>
+      </div>
+    </div>
+  );
+}
