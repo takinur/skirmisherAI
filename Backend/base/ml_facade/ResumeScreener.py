381ff4783a47ae7a6a530d90d5af04a8fbee61cf
Takinur M <takinurm@gmail.com>
2022-10-08 13:15:49 +0600

    Initial Prediction

--- Backend/base/ml_facade/ResumeScreener.py
+++ Backend/base/ml_facade/ResumeScreener.py
@@ -1,4 +1,3 @@
-import pwd
 import sys
 import numpy as np
 import tensorflow as tf
@@ -42,16 +41,19 @@ class resumeScreener:
     
     def screenResume(self, text):
         # Get feature text tokenizer used for model training
-        with open('assets/tokenizer/feature_tokenizer.pickle', 'rb') as handle:
+        tokenizer_path = os.path.join(os.path.dirname(__file__), 'assets/tokenizer/feature_tokenizer.pickle')
+        with open(tokenizer_path, 'rb') as handle:
             feature_tokenizer = pickle.load(handle)
 
         # Get label encoding dictionary from model training
-        with open('assets/dictionary/dictionary.pickle', 'rb') as handle:
+        dic_path = os.path.join(os.path.dirname(__file__), 'assets/dictionary/dictionary.pickle')
+        with open(dic_path, 'rb') as handle:
             encoding_to_label = pickle.load(handle)
 
         # Handle unknown label case and load original labels
         encoding_to_label[0] = 'unknown'
-        with open("assets/data/labels.json", "r") as read_file:
+        label_path = os.path.join(os.path.dirname(__file__), 'assets/data/labels.json')
+        with open(label_path, "r") as read_file:
             original_labels = json.load(read_file)
         
         cleaned_input = self._clean_resume(text)
@@ -82,7 +84,7 @@ class resumeScreener:
 
 
 
-def wrapper (text):
+def wrapper (text, label):
     text  = 'TAKINUR I MAHIM Dhaka, Bangladesh takinurm@gmail.com takinur.com github.com/takinur linkedin.com/in/takinur 2019 - Ongoing B.Sc. (hons.) in Computing University of Greenwich EDUCATION Relevant Coursework: Computing Project, Software Development Technique, Dynamic Website, Human Computer Interaction Design, Object-Oriented Programming, Database Administration PROFESSIONAL EXPERIENCE Full-Stack Web Developer (Freelance) Modified existing websites to correct coding errors, features add and workflow changes. Implemented mobile-first with usable components for better UX. Worked closely with clients to collect requirements and to ensure product quality. Designed and created UI that is optimized and SEO friendly. Configured web servers and deployed production ready website. 2021 - Ongoing SKILLS Programming: Python, JavaScript, TypeScript, PHP, HTML, CSS, SQL Technologies: Docker, Git, Bash, Node.JS, Figma, AWS, Linux, Composer, Vite, RestAPI, Testing Django, FastAPI, React, TailwindCSS, VUE, Laravel, Bootstrap, Next, Nuxt, Express Frameworks: Databases: PostgreSQL, MySQL, Oracle, MongoDB, SQLite Other: English Language, Tensorflow, Pandas, Matplotlib, Agile, OOP, Quick Learner PROJECTS Developed a booking site for consumers to book, manage and track parcel. Implemented HR Management and three-way user management system. Made a customized process for flawless payment with Stripe API. E-Courier Web Application PHP, Laravel, JavaScript, TailwindCSS, MySQL, VUE.JS Reversed Job Platform Python, FastAPI, PHP, Laravel, JavaScript, Bootstrap, MySQL Enabled users to post, manage and view jobs along with skills overview. Made a advance search feature to find and invite preferred candidate to hire. Talent Screening Web APP Python, Django, JavaScript, React, TailwindCSS, PostgreSQL, Redux, TensorFlow Developed a SPA Application with Restful API for automatic Resume Screening. Implemented Machine Leaning Algorithm to analyze and Suitability Prediction. Executed custom Natural Language Processing to extract essential info from Resume. Researched thoroughly and documented the study with background and scopes. Web Developer BIOVAAS TECH AID | Multiplan Center, 69-71 New Elephant Road, Dhaka Built and maintained website for E-Commerce Platform. Assisted troubleshooting software. Modified, extended as per requirements and performed white-box, black-box testing. Filed reports, gathered requirements, and performed research for extensibility. Jan 2020 - Jun 2021'
     rs = resumeScreener()
     return rs.screenResume(text)
@@ -108,7 +110,7 @@ if __name__ == '__main__':
         text = sys.argv[1]
 
     # Extracted data from the resume class
-    data = wrapper(text)
+    data = wrapper(text, 'label')
 
     print(data)
 
