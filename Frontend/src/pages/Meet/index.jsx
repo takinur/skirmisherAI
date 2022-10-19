ae244ccc56cab0cb586f8211a88d5058028faf76
Takinur M <takinurm@gmail.com>
2022-10-19 20:24:02 +0600

    Meet join code input

--- Frontend/src/pages/Meet/index.jsx
+++ Frontend/src/pages/Meet/index.jsx
@@ -5,9 +5,17 @@ import VideoCall from "../../components/meet/VideoCall";
 import { useParams } from "react-router-dom";
 
 export const VideoMeet = () => {
-  const slug = useParams().slug;
+  const [joinCode, setJoinCode] = useState("");
 
-  console.log(slug);
+  const param = useParams();
+
+  const handleChange = (e) => {
+    setJoinCode(e.target.value);
+  };
+
+  useEffect(() => {
+    setJoinCode(param.slug);
+  }, [param]);
 
   const [inCall, setInCall] = useState(false);
 
@@ -18,26 +26,35 @@ export const VideoMeet = () => {
           <VideoCall setInCall={setInCall} />
         ) : (
           <div className="flex h-screen items-center justify-center">
-            <button
-              onClick={() => setInCall(true)}
-              className="ripple flex rounded-full bg-green-700 p-3 text-center text-white shadow transition hover:bg-green-800 hover:shadow-lg focus:outline-none"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-6 w-6"
+            <div class="m-4 flex">
+              <input
+                onChange={handleChange}
+                value={joinCode}
+                required
+                className="mr-0 rounded-l-lg border-t border-b border-l border-gray-200 bg-white p-4 text-gray-800"
+                placeholder="Meet code"
+              />
+              <button
+                onClick={() => setInCall(true)}
+                className="flex rounded-r-lg border-t border-b  border-r border-green-500 bg-green-700 p-4 px-8 font-bold uppercase text-gray-50"
               >
-                <path
-                  strokeLinecap="round"
-                  d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
-                />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+                  />
+                </svg>
 
-              <span className="ml-2">Join Call</span>
-            </button>
+                <span className="ml-2">Join Call</span>
+              </button>
+            </div>
           </div>
         )}
       </div>
