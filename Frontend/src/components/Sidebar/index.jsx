4a158e3453c496a3495ae06cbc9329f9e9cfdd20
Takinur M <takinurm@gmail.com>
2022-10-27 19:18:18 +0600

    SideNav: Changed List Order

--- Frontend/src/components/Sidebar/index.jsx
+++ Frontend/src/components/Sidebar/index.jsx
@@ -1,74 +1,80 @@
-import React from "react";
-import ClassNames from "classnames";
-import { Link, NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
+import React from 'react'
+import ClassNames from 'classnames'
+import { Link, NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const SideBar = ({ navbar, toggleNavbar }) => {
-  const { user } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.auth)
 
-  let MenuItems = [];
+  let MenuItems = []
   //Conditionally render menu items based on user role
-  if (user.role === "EMPLOYER") {
+  if (user.role === 'EMPLOYER') {
     MenuItems = [
       {
-        name: "Dashboard",
-        link: "/dashboard",
+        name: 'Dashboard',
+        link: '/dashboard',
         icon: HomeIcon,
       },
       {
-        name: "Jobs",
-        link: "/employer/jobs",
+        name: 'Jobs',
+        link: '/employer/jobs',
         icon: ListIcon,
       },
       {
-        name: "Reports",
-        link: "/employer/reports",
-        icon: ListIcon,
-      },
-      {
-        name: "Profile",
-        link: "/user/profile",
+        name: 'Profile',
+        link: '/user/profile',
         icon: UserIcon,
       },
       {
-        name: "Setting",
-        link: "/user/setting",
+        name: 'Community',
+        link: '/user/profile',
+        icon: CommunityIcon,
+      },
+      {
+        name: 'Reports',
+        link: '/user/blog',
+        icon: IconCards,
+      },
+
+      {
+        name: 'Setting',
+        link: '/user/setting',
         icon: CogIcon,
       },
       {
-        name: "Help",
-        link: "/help",
+        name: 'Help',
+        link: '/help',
         icon: HelpIcon,
       },
-    ];
-  } else if (user.role === "CANDIDATE") {
+    ]
+  } else if (user.role === 'CANDIDATE') {
     MenuItems = [
       {
-        name: "Dashboard",
-        link: "/dashboard",
+        name: 'Dashboard',
+        link: '/dashboard',
         icon: MenuIcon,
       },
       {
-        name: "Jobs",
-        link: "/applicant/jobs",
+        name: 'Jobs',
+        link: '/applicant/jobs',
         icon: ListIcon,
       },
       {
-        name: "Profile",
-        link: "/user/profile",
+        name: 'Profile',
+        link: '/user/profile',
         icon: UserIcon,
       },
       {
-        name: "Setting",
-        link: "/user/setting",
+        name: 'Setting',
+        link: '/user/setting',
         icon: CogIcon,
       },
       {
-        name: "Help",
-        link: "/help",
+        name: 'Help',
+        link: '/help',
         icon: HelpIcon,
       },
-    ];
+    ]
   }
 
   // console.log(MenuItems);
@@ -78,16 +84,13 @@ const SideBar = ({ navbar, toggleNavbar }) => {
   return (
     <aside
       className={ClassNames(
-        "fixed top-0 left-0 z-30 min-h-screen w-64 flex-shrink-0 overflow-y-auto bg-white dark:bg-gray-800 md:block",
-        navbar ? "block" : "hidden"
+        'fixed top-0 left-0 z-30 min-h-screen w-64 flex-shrink-0 overflow-y-auto bg-white dark:bg-gray-800 md:block',
+        navbar ? 'block' : 'hidden'
       )}
     >
       <div className="py-4 text-gray-500 dark:text-gray-400">
         <div className="flex w-full justify-between pr-2">
-          <Link
-            className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
-            to="/dashboard"
-          >
+          <Link className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" to="/dashboard">
             SkirmisherAI
           </Link>
           <svg
@@ -111,9 +114,7 @@ const SideBar = ({ navbar, toggleNavbar }) => {
             <li className="relative px-6 py-3" key={item.name}>
               <NavLink
                 className={({ isActive }) =>
-                  isActive
-                    ? "side-nav-link [--child-opacity:1]"
-                    : " side-nav-link"
+                  isActive ? 'side-nav-link [--child-opacity:1]' : ' side-nav-link'
                 }
                 to={item.link}
               >
@@ -130,10 +131,10 @@ const SideBar = ({ navbar, toggleNavbar }) => {
         </ul>
       </div>
     </aside>
-  );
-};
+  )
+}
 
-export default SideBar;
+export default SideBar
 
 function HomeIcon() {
   return (
@@ -149,7 +150,7 @@ function HomeIcon() {
     >
       <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
     </svg>
-  );
+  )
 }
 
 function ListIcon() {
@@ -168,7 +169,7 @@ function ListIcon() {
         d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
       />
     </svg>
-  );
+  )
 }
 
 function IconCards() {
@@ -187,7 +188,7 @@ function IconCards() {
         d="M6 6.878V6a2.25 2.25 0 012.25-2.25h7.5A2.25 2.25 0 0118 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 004.5 9v.878m13.5-3A2.25 2.25 0 0119.5 9v.878m0 0a2.246 2.246 0 00-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0121 12v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6c0-.98.626-1.813 1.5-2.122"
       />
     </svg>
-  );
+  )
 }
 
 function UserIcon() {
@@ -206,7 +207,7 @@ function UserIcon() {
         d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
       />
     </svg>
-  );
+  )
 }
 
 function CogIcon() {
@@ -224,13 +225,9 @@ function CogIcon() {
         strokeLinejoin="round"
         d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
       />
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-      />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
     </svg>
-  );
+  )
 }
 
 function HelpIcon() {
@@ -249,7 +246,7 @@ function HelpIcon() {
         d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
       />
     </svg>
-  );
+  )
 }
 
 function MenuIcon() {
@@ -268,5 +265,22 @@ function MenuIcon() {
         d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z"
       />
     </svg>
-  );
+  )
 }
+
+const CommunityIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="h-6 w-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+    />
+  </svg>
+)
