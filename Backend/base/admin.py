f370c96b4031d07e302722ef0f817fab916ebbbd
Takinur M <takinurm@gmail.com>
2022-10-30 01:15:47 +0600

    Admin: Manage USers

--- Backend/base/admin.py
+++ Backend/base/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+# UserAccount model
+from .models import UserAccount
+
+
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'is_superuser',
+                    'is_staff', 'role',)
+    search_fields = ('name', 'email')
+    readonly_fields = ('last_login',)
+    exculude = ('password',)
+
+    list_filter = ('role',)
+    fieldsets = ()
+
+
+admin.site.register(UserAccount, UserAccountAdmin)
