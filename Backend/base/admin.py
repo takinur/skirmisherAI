c8eea50d622f53f77e82270a90b8bded26d3ebfe
Takinur M <takinurm@gmail.com>
2022-10-30 02:26:53 +0600

    Admin: Create/Update Candidate Profile

--- Backend/base/admin.py
+++ Backend/base/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # UserAccount model
-from .models import EmployerProfile, UserAccount
+from .models import CandidateProfile, EmployerProfile, UserAccount
 
 
 class UserAccountAdmin(admin.ModelAdmin):
@@ -29,7 +29,7 @@ class EmployerProfileAdmin(admin.ModelAdmin):
     search_fields = ('user', 'company_name', 'website',
                      'phone', 'location',)
 
-    exculude = ('password', 'last_login', 'updated_at',)
+    # exculude = ('password', 'last_login', 'updated_at',)
 
     list_filter = ('company_name', 'location', 'phone',)
     fieldsets = (
@@ -43,5 +43,21 @@ class EmployerProfileAdmin(admin.ModelAdmin):
     )
 
 
+class CandidateProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'email', 'phone',
+                    'website', 'designation', 'location', 'resume_file', 'created_at')
+    search_fields = ('user', 'email', 'location',
+                     'phone', 'designation',)
+
+    list_filter = ('location', 'designation', 'created_at',)
+    fieldsets = (
+        (None, {
+            'fields': ('resume_file', 'location', 'designation', 'website', 'user')
+        }),
+
+    )
+
+
 admin.site.register(UserAccount, UserAccountAdmin)
 admin.site.register(EmployerProfile, EmployerProfileAdmin)
+admin.site.register(CandidateProfile, CandidateProfileAdmin)
