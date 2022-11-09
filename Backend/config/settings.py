b7dc77ca64f9a42b75275140fab256892128f8ad
Takinur M <takinurm@gmail.com>
2022-11-10 02:37:55 +0600

    Site Logo: Admins

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -222,15 +222,15 @@ JAZZMIN_SETTINGS = {
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_brand": "SkirmisherAI",
     # Logo to use for your site, must be present in static files, used for brand on top left
-    # "site_logo": "books/img/logo.png",
+    "site_logo": "assets/logo-56.png",
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": None,
+    "login_logo": "assets/logo-56.png",
     # Logo to use for login form in dark themes (defaults to login_logo)
-    "login_logo_dark": None,
+    "login_logo_dark": "assets/logo-56.png",
     # CSS classes that are applied to the logo above
     "site_logo_classes": "img-circle",
     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
-    "site_icon": None,
+    "site_icon": "assets/favico.ico",
     # Welcome text on the login screen
     "welcome_sign": "Welcome to the SkirmisherAI",
     # Copyright on the footer
