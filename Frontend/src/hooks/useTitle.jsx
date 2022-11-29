14a08b1a8a8d264b6b5e804f4aa7695ea2ce246a
Takinur M <takinurm@gmail.com>
2022-11-29 10:57:34 +0600

    Scroll To Top: Through Hook

--- Frontend/src/hooks/useTitle.jsx
+++ Frontend/src/hooks/useTitle.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const useTitle = (title) => {
   return React.useEffect(() => {
+    //Scroll to top of page on load
+    window.scrollTo(0, 0)
     document.title = title ? `${title} - SkirmisherAI` : 'SkirmisherAI'
   }, [title])
 }
